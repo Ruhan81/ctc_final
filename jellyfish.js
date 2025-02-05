44236455b009c628da615150706bff524566158c
Ruhan81 <ruruzgreat@gmail.com>
2025-02-05 04:08:32 -0500

    Updates

--- jellyfish.js
+++ jellyfish.js
@@ -1,7 +1,6 @@
 import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
 import { GLTFLoader } from "https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js";
 
-// Scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
